4d9450aaefa77997be881ddabaac253a84f54a30
dlievre <42@photeam.com>
2016-12-14 15:38:56 +0100

    Add files via upload

--- ft_tblinit.c
+++ ft_tblinit.c
@@ -6,13 +6,17 @@
 /*   By: dlievre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 14:36:57 by dlievre           #+#    #+#             */
-/*   Updated: 2016/12/14 14:38:19 by dlievre          ###   ########.fr       */
+/*   Updated: 2016/12/14 15:34:11 by dlievre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void			ft_tblinit(int	*tbl, size_t len)
+/*
+** permet d'initialiser a \0 chaque variable des tableaux[n]
+*/
+
+void			ft_tblinit(int *tbl, size_t len)
 {
 	while (len-- > 0)
 		*tbl++ = '\0';
