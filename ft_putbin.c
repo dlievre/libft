78652e5062cd5404dd2178d1eebf5180495912bc
dlievre <42@photeam.com>
2016-12-15 17:40:15 +0100

    ft_putbin ft_putbin32 validés avec juste nl
    ft_putbin ft_putbin32 validés en masq 32bits avec juste nl

--- ft_putbin.c
+++ ft_putbin.c
@@ -6,48 +6,52 @@
 /*   By: dlievre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 18:46:15 by dlievre           #+#    #+#             */
-/*   Updated: 2016/12/15 13:39:31 by dlievre          ###   ########.fr       */
+/*   Updated: 2016/12/15 17:37:51 by dlievre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 /*
-** affiche en binaire la variable passee, sous 2, 4 ou 8 octeds
-** 2 octeds = 0xFF, 4 = 0xFFFF ou 8 0xFFFFFFFF octeds
-** 2 = 8 bits 0b00000000, 4 = 16 bits 0b0000000000000000, 8 = 32bits
+** affiche en binaire la variable passee, sous 4 ou 8  octeds
+** 4 = 0xFFFF ou 8 0xFFFFFFFF octeds
+** 4 = 16 bits 0b0000000000000000, 8 = 32bits
+** pour obtenir un mask de 32 bits on decale le mask a 1 de 32 fois a gauche
 */
 
-int		ft_putbin(unsigned bin, int octed, int nl)
+#include "libft.h"
+
+int		ft_putbin(unsigned bin, int nl)
 {
-	long mask;
-	int nbit;
+	int mask;
+	int n;
 
-	if (!bin || (octed != 2 && octed != 4 && octed != 8))
-		return (1);
-	if (octed == 2)
+	mask = 0x8000;
+	n = 16;
+	while (n-- > 0)
 	{
-		mask = 0x8000;
-		nbit = 8;
-	}
-	if (octed == 4)
-	{
-		mask = 0x80000000;
-		nbit = 16;
-	}
-	if (octed == 8)
-	{
-		mask = 0x8000000000000000;
-		nbit = 32;
-	}
-
-	while (nbit-- > 0)
-	{
-		ft_putchar((mask & bin) == mask ? '1' : '0');
-	bin = bin << 1;
+		ft_putchar((mask & bin) == 0x8000 ? '1' : '0');
+		bin = bin << 1;
 	}
 	if (nl == 1)
 		ft_putchar('\n');
+	return (0);
+}
 
+int		ft_putbin32(unsigned long bin, int nl)
+{
+	unsigned long mask;
+	int n;
+
+	mask = 1;
+	n = 0;
+	while (n++ < 32)
+		mask = mask << 1;
+//	n = 32;
+	while (n-- > 0)
+	{
+		ft_putchar((mask & bin) == mask ? '1' : '0');
+		bin = bin << 1;
+	}
+	if (nl == 1)
+		ft_putchar('\n');
 	return (0);
 }
