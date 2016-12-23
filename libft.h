6b486eb223cc86d19d6c3515ee2287d6a98e2cf0
dlievre <42@photeam.com>
2016-12-23 17:17:47 +0100

    ajout putcharendl

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dlievre <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 18:03:00 by dlievre           #+#    #+#             */
-/*   Updated: 2016/12/22 15:16:44 by dlievre          ###   ########.fr       */
+/*   Updated: 2016/12/23 11:31:56 by dlievre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,4 +92,5 @@ int					ft_putbin(unsigned int bin, int nbits, char c);
 void				ft_tblinit(int *tbl, size_t len);
 int					ft_putchartohex(char str);
 void				ft_putstrstr(char *bfrmdlaft, char const *f, const char *s);
+void				ft_putcharendl(char c);
 #endif
